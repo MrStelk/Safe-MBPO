e5799d52c5630db134ae856665a3853095b74311
MrStelk <karthikndasaraju@iitrpr.ac.in>
2025-03-09 21:13:44 +0000

    changing results path and cuda

--- src/defaults.py
+++ src/defaults.py
@@ -7,5 +7,4 @@ ACTOR_LR = 3e-4
 CRITIC_LR = 1e-3
 
 # ROOT_DIR = None    # set a path (directory) where experiments should be saved
-ROOT_DIR =os.getcwd()
-#ROOT_DIR = '/tiger/u/gwthomas/data/smbpo'
+ROOT_DIR ='/home/rahul/smbpo_k/Safe-MBPO/results/original'

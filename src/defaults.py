9e5a136a3d080fd1d800b1cc74184a8a93f5e33b
MrStelk <karthikndasaraju@iitrpr.ac.in>
2025-03-11 19:57:03 +0000

    branched original

--- src/defaults.py
+++ src/defaults.py
@@ -7,5 +7,5 @@ ACTOR_LR = 3e-4
 CRITIC_LR = 1e-3
 
 # ROOT_DIR = None    # set a path (directory) where experiments should be saved
-ROOT_DIR =os.getcwd()
+ROOT_DIR ='/home/rahul/smbpo_k/Safe-MBPO/results/ismbpo'
 #ROOT_DIR = '/tiger/u/gwthomas/data/smbpo'

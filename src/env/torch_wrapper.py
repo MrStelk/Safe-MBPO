bc5bd938c9855d052d5943aef177bda9988737dc
rahulnarava <134863004+rahulnarava@users.noreply.github.com>
2025-08-09 11:34:49 +0530

    Update torch_wrapper.py

--- src/env/torch_wrapper.py
+++ src/env/torch_wrapper.py
@@ -1,5 +1,5 @@
 from gym import Wrapper
-import time
+#import time
 from ..torch_util import torchify, numpyify
 
 
@@ -8,6 +8,6 @@ class TorchWrapper(Wrapper):
         return torchify(self.env.reset())
 
     def step(self, action):
-        time.sleep(.002)
+        #time.sleep(.002)
         observation, reward, done, info = self.env.step(numpyify(action))
         return torchify(observation), float(reward), done, info
